99b6a4f7f429ef8064b53ed6aa9b397d3b9bc268
Petr Vorel <pvorel@suse.cz>
2017-02-15 14:34:52 +0100

    Add copyright, update year

--- lib/main_common.pm
+++ lib/main_common.pm
@@ -1,3 +1,12 @@
+# SUSE's openQA tests
+#
+# Copyright © 2016-2017 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
 package main_common;
 use base Exporter;
 use File::Basename;
