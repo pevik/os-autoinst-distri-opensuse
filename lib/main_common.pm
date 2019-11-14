pevik/os-autoinst-distri-opensuse

commit 5d4e80ac2a271034e7a96f0c02cc7c9fd94d7f80

LTP: Fix get_ltp_tag() for non-master branches
LTP_RELEASE is defined only for install_ltp.pm. Expecting it's defined
for the tests leads to using wrong tag (=> searching for wrong file)
for non-master branches.

That leads going back to previous version for QEMU based tests:
* PUBLISH_HDD_1 must be used for install_ltp.pm
* HDD_1 must be used for actual tests.

Bare metal tests (IPMI based), which don't use qcow images, need to use
complicated way of tag using DISTRI, VERSION, ... variables (for both
install_ltp.pm and actual tests). This version is not able to
distinguish between different git branches in the same architecture,
that's why it's not used for QEMU based tests (see poo#35041).

Fixes: bca493a1b ("LTP: Adjust tags for bare metal tests")

lib/main_common.pm
@@ -238,8 +238,12 @@ sub is_kernel_test {
 
 sub get_ltp_tag {
     my $tag = get_var('LTP_RUNTEST_TAG');
-    if (!defined $tag) {
-        $tag = get_var('DISTRI') . '-' . get_var('VERSION') . '-' . get_var('ARCH') . '-' . get_var('BUILD') . '-' . get_var('FLAVOR') . '-' . get_var('LTP_RELEASE', 'master') . '@' . get_var('MACHINE');
+    if (!defined $tag && defined get_var('HDD_1')) {
+        $tag = get_var('PUBLISH_HDD_1');
+        $tag = get_var('HDD_1') if (!defined $tag);
+        $tag = basename($tag);
+    } else {
+        $tag = get_var('DISTRI') . '-' . get_var('VERSION') . '-' . get_var('ARCH') . '-' . get_var('BUILD') . '-' . get_var('FLAVOR') . '@' . get_var('MACHINE');
     }
     return $tag . '.txt';
 }

commit 24e695ebc1c413c83f428a4d26633e2e88fb3790

LTP: Respect LTP_RUNTEST_TAG
LTP_RUNTEST_TAG was overwritten with bare metal tests version

Fixes: 5d4e80ac2 ("LTP: Fix get_ltp_tag() for non-master branches")

lib/main_common.pm
@@ -264,12 +264,15 @@ sub is_kernel_test {
 
 sub get_ltp_tag {
     my $tag = get_var('LTP_RUNTEST_TAG');
-    if (!defined $tag && defined get_var('HDD_1')) {
-        $tag = get_var('PUBLISH_HDD_1');
-        $tag = get_var('HDD_1') if (!defined $tag);
-        $tag = basename($tag);
-    } else {
-        $tag = get_var('DISTRI') . '-' . get_var('VERSION') . '-' . get_var('ARCH') . '-' . get_var('BUILD') . '-' . get_var('FLAVOR') . '@' . get_var('MACHINE');
+
+    if (!defined $tag) {
+        if (defined get_var('HDD_1')) {
+            $tag = get_var('PUBLISH_HDD_1');
+            $tag = get_var('HDD_1') if (!defined $tag);
+            $tag = basename($tag);
+        } else {
+            $tag = get_var('DISTRI') . '-' . get_var('VERSION') . '-' . get_var('ARCH') . '-' . get_var('BUILD') . '-' . get_var('FLAVOR') . '@' . get_var('MACHINE');
+        }
     }
     $tag =~ s/[^a-zA-Z0-9_@]+/-/g;
     return $tag . '.txt';

tests/kernel/run_ltp.pm
@@ -428,7 +428,7 @@ E.g.: key=value,key2="value with spaces",key3='another value with spaces'
 
 The runtest asset files are appended with git or pkg depending on how LTP was
 installed. By default the runtest files from the git installation will be
-uesd, but setting this variable to pkg allows that behavior to be overridden.
+used, but setting this variable to pkg allows that behavior to be overridden.
 
 =cut
 