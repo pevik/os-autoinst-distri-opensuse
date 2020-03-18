3f7c5581c214919749548deab17450a81ab22de7
Petr Vorel <pvorel@suse.cz>
2020-03-18 09:40:53 +0100

    LTP: Migrate bare metal fixes to common place
    which enables this fix for openSUSE. Although IPMI is not yet used in
    o3, be prepared for future.
    
    Fixes: d83486058 ("LTP: Introduce bare metal tests")

--- lib/main_ltp.pm
+++ lib/main_ltp.pm
@@ -18,31 +18,51 @@
 package main_ltp;
 use base 'Exporter';
 use Exporter;
-use testapi qw(check_var get_var);
+use testapi qw(check_var get_var set_var);
 use autotest;
 use Archive::Tar;
 use utils;
 use LTP::TestInfo 'testinfo';
 use File::Basename 'basename';
-use main_common qw(load_bootloader_s390x boot_hdd_image get_ltp_tag);
+use version_utils qw(is_released is_sle);
+use main_common qw(boot_hdd_image get_ltp_tag load_ayinst_tests load_boot_tests load_bootloader_s390x load_inst_tests load_reboot_tests loadtest);
 use 5.018;
 use Utils::Backends 'is_pvm';
 # FIXME: Delete the "## no critic (Strict)" line and uncomment "use warnings;"
 # use warnings;
 
 our @EXPORT = qw(
+  load_baremetal_tests
   load_kernel_tests
   loadtest_from_runtest_file
 );
 
-sub loadtest {
+sub load_kernel_test {
     my ($test, %args) = @_;
     autotest::loadtest("tests/kernel/$test.pm", %args);
 }
 
+sub load_baremetal_tests {
+    set_var('ADDONURL', 'sdk')               if (is_sle('>=12') && is_sle('<15')) && !is_released;
+    loadtest 'tests/kernel/ibtests_barriers' if get_var('IBTESTS');
+    loadtest "autoyast/prepare_profile"      if get_var "AUTOYAST_PREPARE_PROFILE";
+    if (get_var('IPXE')) {
+        loadtest 'installation/ipxe_install';
+        loadtest "console/suseconnect_scc";
+    } else {
+        load_boot_tests();
+        get_var("AUTOYAST") ? load_ayinst_tests() : load_inst_tests();
+        load_reboot_tests();
+    }
+    # make sure we always have the toolchain installed
+    loadtest "toolchain/install";
+    # some tests want to build and run a custom kernel
+    loadtest "kernel/build_git_kernel" if get_var('KERNEL_GIT_TREE');
+}
+
 sub shutdown_ltp {
-    loadtest('proc_sys_dump') if get_var('PROC_SYS_DUMP');
-    loadtest('shutdown_ltp', @_);
+    load_kernel_test('proc_sys_dump') if get_var('PROC_SYS_DUMP');
+    load_kernel_test('shutdown_ltp', @_);
 }
 
 sub parse_openposix_runfile {
@@ -54,7 +74,7 @@ sub parse_openposix_runfile {
         if ($line =~ m/$cmd_pattern/ && !($line =~ m/$cmd_exclude/)) {
             my $test  = {name => basename($line, '.run-test'), command => $line};
             my $tinfo = testinfo($test_result_export, test => $test, runfile => $name);
-            loadtest('run_ltp', name => $test->{name}, run_args => $tinfo);
+            load_kernel_test('run_ltp', name => $test->{name}, run_args => $tinfo);
         }
     }
 }
@@ -72,13 +92,13 @@ sub parse_runtest_file {
             my $test  = {name => $1, command => $2};
             my $tinfo = testinfo($test_result_export, test => $test, runfile => $name);
             if ($test->{name} =~ m/$cmd_pattern/ && !($test->{name} =~ m/$cmd_exclude/)) {
-                loadtest('run_ltp', name => $test->{name}, run_args => $tinfo);
+                load_kernel_test('run_ltp', name => $test->{name}, run_args => $tinfo);
             }
         }
     }
 }
 
-sub loadtest_from_runtest_file {
+sub load_kernel_test_from_runtest_file {
     my $namelist           = get_var('LTP_COMMAND_FILE');
     my $path               = shift || get_var('ASSETDIR') . '/other';
     my $unpack_path        = './runtest-files';
@@ -90,9 +110,9 @@ sub loadtest_from_runtest_file {
         environment => {},
         results     => []};
 
-    loadtest('boot_ltp', run_args => testinfo($test_result_export));
+    load_kernel_test('boot_ltp', run_args => testinfo($test_result_export));
     if (get_var('LTP_COMMAND_FILE') =~ m/ltp-aiodio.part[134]/) {
-        loadtest 'create_junkfile_ltp';
+        load_kernel_test 'create_junkfile_ltp';
     }
 
     mkdir($unpack_path, 0755);
@@ -113,7 +133,7 @@ sub loadtest_from_runtest_file {
     shutdown_ltp(run_args => testinfo($test_result_export));
 }
 
-# Replace loadtest_from_runtest_file with this to stress test reverting to
+# Replace load_kernel_test_from_runtest_file with this to stress test reverting to
 # snapshots
 sub stress_snapshots {
     my $count = 100;
@@ -121,83 +141,87 @@ sub stress_snapshots {
     for (my $i = 0; $i < $count / 2; $i++) {
         # This will always fail and revert to the previous milestone, which
         # will either be boot_ltp or write_random#$i
-        loadtest('run_ltp');
-        loadtest('write_random');
+        load_kernel_test('run_ltp');
+        load_kernel_test('write_random');
     }
 }
 
 sub load_kernel_tests {
+    if (get_var('LTP_BAREMETAL') && get_var('INSTALL_LTP')) {
+        load_baremetal_tests();
+    }
+
     load_bootloader_s390x();
-    loadtest "../installation/bootloader" if is_pvm;
+    load_kernel_test "../installation/bootloader" if is_pvm;
 
     if (get_var('INSTALL_LTP')) {
         if (get_var('INSTALL_KOTD')) {
-            loadtest 'install_kotd';
+            load_kernel_test 'install_kotd';
         }
         elsif (get_var('CHANGE_KERNEL_REPO') ||
             get_var('CHANGE_KERNEL_PKG') ||
             get_var('ASSET_CHANGE_KERNEL_RPM')) {
-            loadtest 'change_kernel';
+            load_kernel_test 'change_kernel';
         }
         if (get_var('FLAVOR', '') =~ /Incidents-Kernel/) {
-            loadtest 'update_kernel';
+            load_kernel_test 'update_kernel';
         }
-        loadtest 'install_ltp';
+        load_kernel_test 'install_ltp';
         # If LTP_COMMAND_FILE is set, boot_ltp() and shutdown_ltp() will be added
         # later by install_ltp task.
         if (!get_var('LTP_COMMAND_FILE')) {
             if (get_var('LTP_INSTALL_REBOOT')) {
-                loadtest 'boot_ltp';
+                load_kernel_test 'boot_ltp';
             }
             shutdown_ltp();
         }
     }
     elsif (get_var('LTP_COMMAND_FILE')) {
         if (get_var('INSTALL_KOTD')) {
-            loadtest 'install_kotd';
+            load_kernel_test 'install_kotd';
         }
         elsif (get_var('CHANGE_KERNEL_REPO') ||
             get_var('CHANGE_KERNEL_PKG') ||
             get_var('ASSET_CHANGE_KERNEL_RPM')) {
-            loadtest 'change_kernel';
+            load_kernel_test 'change_kernel';
         }
 
-        loadtest_from_runtest_file();
+        load_kernel_test_from_runtest_file();
     }
     elsif (get_var('QA_TEST_KLP_REPO')) {
         if (get_var('INSTALL_KOTD')) {
-            loadtest 'install_kotd';
+            load_kernel_test 'install_kotd';
         }
-        loadtest 'boot_ltp';
-        loadtest 'qa_test_klp';
+        load_kernel_test 'boot_ltp';
+        load_kernel_test 'qa_test_klp';
     }
     elsif (get_var('VIRTIO_CONSOLE_TEST')) {
-        loadtest 'virtio_console';
-        loadtest 'virtio_console_long_output';
+        load_kernel_test 'virtio_console';
+        load_kernel_test 'virtio_console_long_output';
     }
     elsif (get_var('NVMFTESTS')) {
         boot_hdd_image();
-        loadtest 'nvmftests';
+        load_kernel_test 'nvmftests';
     }
     elsif (get_var('TRINITY')) {
         if (get_var('INSTALL_KOTD')) {
-            loadtest 'install_kotd';
+            load_kernel_test 'install_kotd';
         }
         elsif (get_var('CHANGE_KERNEL_REPO') ||
             get_var('CHANGE_KERNEL_PKG') ||
             get_var('ASSET_CHANGE_KERNEL_RPM')) {
-            loadtest 'change_kernel';
+            load_kernel_test 'change_kernel';
         }
         else {
             boot_hdd_image();
         }
-        loadtest "trinity";
+        load_kernel_test "trinity";
     } elsif (get_var('NUMA_IRQBALANCE')) {
         boot_hdd_image();
-        loadtest 'numa_irqbalance';
+        load_kernel_test 'numa_irqbalance';
     }
 
     if (check_var('BACKEND', 'svirt') && get_var('PUBLISH_HDD_1')) {
-        loadtest '../shutdown/svirt_upload_assets';
+        load_kernel_test '../shutdown/svirt_upload_assets';
     }
 }
