e2b16ee3bdade5dd7dffb562d67ead55c1116ce0
Petr Vorel <pvorel@suse.cz>
2021-01-14 10:40:17 +0100

    DEVEL: disable update_kernel.pm
    For testing QAM.

--- lib/main_ltp.pm
+++ lib/main_ltp.pm
@@ -50,9 +50,9 @@ sub load_kernel_tests {
             get_var('ASSET_CHANGE_KERNEL_RPM')) {
             loadtest_kernel 'change_kernel';
         }
-        if (get_var('FLAVOR', '') =~ /Incidents-Kernel/) {
-            loadtest_kernel 'update_kernel';
-        }
+        #if (get_var('FLAVOR', '') =~ /Incidents-Kernel/) {
+        #    loadtest_kernel 'update_kernel';
+        #}
         loadtest_kernel 'install_ltp';
         # If there is a command file then install_ltp schedules boot_ltp which
         # will schedule shutdown
