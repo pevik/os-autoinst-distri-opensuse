pevik/os-autoinst-distri-opensuse

commit 2a92601b45b9e89ca77527ff4fbc680230d9a37c

Add target to run full check of unused modules
As per review comments, we might still want to run check against all
files, which we can do once there is a change in master branch for
instance.

Makefile
@@ -108,7 +108,7 @@ test-spec:
 	tools/update_spec --check
 
 .PHONY: test-static
-test-static: tidy-check test-yaml-valid test-modules-in-yaml-schedule test-merge test-dry test-no-wait_idle test-deleted-renamed-referenced-files detect-nonexistent-testdata test-unused-modules test-soft_failure-no-reference test-spec test-invalid-syntax
+test-static: tidy-check test-yaml-valid test-modules-in-yaml-schedule test-merge test-dry test-no-wait_idle test-deleted-renamed-referenced-files detect-nonexistent-testdata test-unused-modules-changed test-soft_failure-no-reference test-spec test-invalid-syntax
 .PHONY: test
 ifeq ($(TESTS),compile)
 test: test-compile
@@ -126,11 +126,16 @@ PERLCRITIC=PERL5LIB=tools/lib/perlcritic:$$PERL5LIB perlcritic --quiet --stern -
 perlcritic: tools/lib/
 	${PERLCRITIC} $$(git ls-files -- '*.p[ml]' ':!:data/')
 
-.PHONY: test-unused-modules
-test-unused-modules:
+.PHONY: test-unused-modules-changed
+test-unused-modules-changed:
+	@echo "[make] Unused modules check called over modified/new files only. For a full run use make test-unused-modules-full"
 	tools/detect_unused_modules `git diff --name-only --diff-filter=d $$(git merge-base master HEAD) | grep '^tests/*'`
 	tools/detect_unused_modules `git diff --unified=0 $$(git merge-base master HEAD) products/* | grep -oP "^-.*loadtest.*[\"']\K[^\"'].+(?=[\"'])"`
 	tools/detect_unused_modules `git diff --unified=0 $$(git merge-base master HEAD) schedule/* | grep -oP "^-\s+- [\"']?\K.*(?=[\"']?)" | grep -v '{{'`
+
+.PHONY: test-unused-modules
+test-unused-modules:
+	tools/detect_unused_modules
 
 .PHONY: test-deleted-renamed-referenced-files
 test-deleted-renamed-referenced-files:

tools/detect_unused_modules
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-MODULES="${@}"
+MODULES="${@}:=$(git ls-files "*tests/*.pm")"
 
 for test in $MODULES ; do
     t=$(echo $test | sed -e 's@tests/.*/@@' -e 's@\.pm$@@')

commit d119e048cc7ff97e200b2a708df5db5e9bc7e600

Makefile: Rename target to test-unused-modules-full
Fixes: 2a92601b4 ("Add target to run full check of unused modules")

Signed-off-by: Petr Vorel <pvorel@suse.cz>

Makefile
@@ -137,8 +137,8 @@ test-unused-modules-changed:
 	git --no-pager diff --unified=0 origin/master schedule/* | sed -n "s~^-\s\+-\s\+\([\"']\)\([^\"']\+\)\1.*~tests/\2.pm~p" | grep -v '{{' ) \
 	| sort -u`
 
-.PHONY: test-unused-modules
-test-unused-modules:
+.PHONY: test-unused-modules-full
+test-unused-modules-full:
 	tools/detect_unused_modules -a
 
 .PHONY: test-deleted-renamed-referenced-files