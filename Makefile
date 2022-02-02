pevik/os-autoinst-distri-opensuse

commit 5cdb32775f549a31c4e17e5cc818e30419846da2

Fix detect_unused_modules when file is renamed or removed
Script throws false negatives if module was removed from the schedule
and renamed or completely removed. To avoid this, we now check if file
exists before reporting it as unused module.

Makefile
@@ -130,9 +130,9 @@ perlcritic: tools/lib/
 .PHONY: test-unused-modules-changed
 test-unused-modules-changed:
 	@echo "[make] Unused modules check called over modified/new files only. For a full run use make test-unused-modules-full"
-	tools/detect_unused_modules -m `git diff --name-only --diff-filter=d $$(git merge-base master HEAD) | grep '^tests/*'`
-	tools/detect_unused_modules -m `git diff --unified=0 $$(git merge-base master HEAD) products/* | grep -oP "^-.*loadtest.*[\"']\K[^\"'].+(?=[\"'])"`
-	tools/detect_unused_modules -m `git diff --unified=0 $$(git merge-base master HEAD) schedule/* | grep -oP "^-\s+- [\"']?\K.*(?=[\"']?)" | grep -v '{{'`
+	tools/detect_unused_modules -m `git --no-pager diff --name-only --diff-filter=d $$(git merge-base master HEAD) | grep '^tests/*'`
+	tools/detect_unused_modules -m `git --no-pager diff --unified=0 $$(git merge-base master HEAD) products/* | grep -oP "^-.*loadtest.*[\"']\K[^\"'].+(?=[\"'])"`
+	tools/detect_unused_modules -m `git --no-pager diff --unified=0 $$(git merge-base master HEAD) schedule/* | grep -oP "^-\s+- [\"']?\K.*(?=[\"']?)" | grep -v '{{'`
 
 .PHONY: test-unused-modules
 test-unused-modules:

tools/detect_unused_modules
@@ -36,6 +36,10 @@ while [ "$#" -gt 0 ]; do
 done
 
 for test in $MODULES ; do
+    # Don't check non-existing modules it as it was either renamed or removed
+    if [ -z `git ls-files "*$test*"` ]; then
+        continue
+    fi
     t=$(basename $test | sed -e 's@\.pm$@@')
     t_dir=$(dirname $test | sed -e 's@tests/\(.*\)$@\1@')
     git grep -E "(- $t_dir/$t|loadtest.*$t|load_testdir.*$t_dir|^use (base )?\"?$t\"?)" | grep -qv ".(pm|yaml):\s*#" || echo "$test module is not used in any schedule"

commit 5338e3a88d0d12fb9a190d3afd18fc57f1eb5698

detect_unused_modules: Fix module detection
Both the original code in 5cdb32775 and attempt to fix it 407cfa0d9 were
missing required quotes. Thus after 407cfa0d9 we got error:

tools/detect_unused_modules: 40: [: tests/containers/docker.pm: unexpected operator
tools/detect_unused_modules: 40: [: tests/containers/podman.pm: unexpected operator
tools/detect_unused_modules: 40: [: tests/containers/docker.pm: unexpected operator
...

not only on CI (we use ubuntu-latest, which has dash as the default
shell which is more strict) but also on Tumbleweed with bash as the
default shell. It's clear that people blindly rely on CI being green
(don't run tests locally, don't look at the log output in CI when
changing things).

Moreover 407cfa0d9 added redundant 'tests/' (directory already included
in $MODULES, when called with -a), therefore nothing was checked due
invalid path 'tests/tests/...', because that was needed for -m MODULES
usage.

Therefore make 'tests/' mandatory, i.e. use relative path to test
module (well, both 'tests/' and .pm extension are then removed) and thus
put it into -m in Makefile.

Also remove duplicity and call tools/detect_unused_modules only once.
This speedup things a lot.

NOTE: whole script has a messy design and it's hard to debug, it'd
deserve redesign.

Fixes: 5cdb32775 ("Fix detect_unused_modules when file is renamed or removed")
Fixes: 407cfa0d9 ("Fix non-existent test modules check")

Signed-off-by: Petr Vorel <pvorel@suse.cz>

Makefile
@@ -131,9 +131,11 @@ perlcritic: tools/lib/
 .PHONY: test-unused-modules-changed
 test-unused-modules-changed:
 	@echo "[make] Unused modules check called over modified/new files only. For a full run use make test-unused-modules-full"
-	tools/detect_unused_modules -m `git --no-pager diff --name-only --diff-filter=d origin/master | grep '^tests/*' | grep -v '^tests/test_pods/'`
-	tools/detect_unused_modules -m `git --no-pager diff --unified=0 origin/master products/* | grep -oP "^-.*loadtest.*[\"']\K[^\"'].+(?=[\"'])"`
-	tools/detect_unused_modules -m `git --no-pager diff --unified=0 origin/master schedule/* | grep -oP "^-\s+- [\"']?\K.*(?=[\"']?)" | grep -v '{{'`
+	tools/detect_unused_modules -m ` (\
+	git --no-pager diff --name-only --diff-filter=d origin/master | grep '^tests/*' | grep -v '^tests/test_pods/'; \
+	git --no-pager diff --unified=0 origin/master products/* | sed -n "s~^-.*loadtest\s\+\([\"']\)\([^\"']\+\)\1.*~tests/\2.pm~p"; \
+	git --no-pager diff --unified=0 origin/master schedule/* | sed -n "s~^-\s\+-\s\+\([\"']\)\([^\"']\+\)\1.*~tests/\2.pm~p" | grep -v '{{' ) \
+	| sort -u`
 
 .PHONY: test-unused-modules
 test-unused-modules:

tools/detect_unused_modules
@@ -2,11 +2,11 @@
 
 usage="Detect test modules usage in the schedules, inluding main.pm and yaml schedules.
 
-Usage: $(basename "$0") { -a | -m [modules] } | -h
+Usage: $(basename "$0") { -a | -m MODULES } | -h
 
     -h|--help                 show this help text
     -a|--all                  check all test modules
-    -m|--modules [modules]    check provided list"
+    -m|--modules MODULES      check provided list (module names must contain 'tests/', i.e. relative path)"
 
 if [ $# -eq 0 ]; then
     echo "$usage"
@@ -37,9 +37,11 @@ done
 
 for test in $MODULES ; do
     # Don't check non-existing modules it as it was either renamed or removed
-    if [ -z `git ls-files "tests/$test*"` ]; then
-        continue
-    fi
+    # also check only for tests in tests/ directory
+    git ls-files "*$test*" | grep -q ^tests/ || continue
+
+    echo "check $test" >&2
+
     t=$(basename $test | sed -e 's@\.pm$@@')
     t_dir=$(dirname $test | sed -e 's@tests/\(.*\)$@\1@')
     git grep -E "(- $t_dir/$t|loadtest.*$t|load_testdir.*$t_dir|^use (base )?\"?$t\"?)" | grep -qv ".(pm|yaml):\s*#" || echo "$test module is not used in any schedule"