510f8906fd389df2b1d1eb701f3e69aacd912ccb
Petr Vorel <pvorel@suse.cz>
2023-07-26 16:58:10 +0200

    host_config: Print repos, disable adding repo
    It fixes:
    zypper -n ar http://openqa.suse.de/assets/repo/ALP-Dolomite-1.0-x86_64-Build2.4-Media1 'ALP Build Repository' failed with code 4
    due Repository named 'ALP Build Repository' already exists. Please use
    another alias.
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- tests/transactional/host_config.pm
+++ tests/transactional/host_config.pm
@@ -33,9 +33,12 @@ sub run {
     }
 
     if (is_alp) {
+        record_info('zypper lr', script_output('zypper lr -u', proceed_on_failure => 1));
+
         # Add additional ALP repositories
         my $repo = get_required_var('REPO_SLE_ALP');
-        zypper_call("ar http://openqa.suse.de/assets/repo/$repo 'ALP Build Repository'");
+        #zypper_call("ar http://openqa.suse.de/assets/repo/$repo 'ALP Build Repository'");
+
         my $source_repo = get_var('REPO_ALP_SOURCE_BUILD');
         zypper_call("ar $source_repo 'ALP Source Build Repository'") if $source_repo;
         zypper_call("--gpg-auto-import-keys ref");
