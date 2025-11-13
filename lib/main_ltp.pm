eb73bdcbd609abfec76e21993356b05b91d6fc70
Petr Vorel <pvorel@suse.cz>
2025-11-13 13:42:01 +0100

    TESTING PURPOSES ONLY
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- lib/main_ltp.pm
+++ lib/main_ltp.pm
@@ -59,18 +59,18 @@ sub load_kernel_tests {
             get_var('ASSET_CHANGE_KERNEL_RPM')) {
             loadtest_kernel 'change_kernel';
         }
-        if (get_var('FLAVOR', '') =~ /Incidents-Kernel|Updates-Staging|Increments|Maintenance-KOTD/) {
-            loadtest_kernel 'update_kernel';
-        }
+        #if (get_var('FLAVOR', '') =~ /Incidents-Kernel|Updates-Staging|Increments|Maintenance-KOTD/) {
+        #   loadtest_kernel 'update_kernel';
+        #}
 
         # transactional needs to first run install_ltp due broken grub menu
         # counting detection in add_custom_grub_entries():
         # Test died: Unexpected number of grub entries: 5, expected: 3 at lib/bootloader_setup.pm line 166.
         my $needs_update = is_transactional && (get_var('FLAVOR', '') =~ /-Staging|-Updates/);
 
-        if ($needs_update && get_var('KGRAFT')) {
-            loadtest_kernel 'update_kernel';
-        }
+        #if ($needs_update && get_var('KGRAFT')) {
+        #   loadtest_kernel 'update_kernel';
+        #}
 
         loadtest_kernel 'install_ltp';
 
