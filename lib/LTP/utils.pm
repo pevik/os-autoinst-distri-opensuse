4234590ec88b71aae315aa9977a1441dd4f30da6
Petr Vorel <pvorel@suse.cz>
2026-01-09 09:49:03 +0100

    LTP: Log HPET
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- lib/LTP/utils.pm
+++ lib/LTP/utils.pm
@@ -106,6 +106,9 @@ sub log_versions {
     my $kernel_config = script_output('for f in "/boot/config-$(uname -r)" "/usr/lib/modules/$(uname -r)/config" /proc/config.gz; do if [ -f "$f" ]; then echo "$f"; break; fi; done');
     my $run_cmd = is_transactional ? 'transactional-update -c run ' : '';
 
+    record_info('HPET', script_output('grep HPET /proc/driver/rtc || true'));
+    record_info('HPET2', script_output('cat /proc/driver/rtc || true'));
+
     script_run("$run_cmd rpm -qi $kernel_pkg > $kernel_pkg_log 2>&1", timeout => 120);
     upload_logs($kernel_pkg_log, failok => 1);
 
