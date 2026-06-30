a77f9f25dc38623d570fb5625fa68d9bd5d61b98
Petr Vorel <pvorel@suse.cz>
2026-06-30 17:08:44 +0200

    LTP: Compile irqbalance01.c
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- lib/LTP/utils.pm
+++ lib/LTP/utils.pm
@@ -45,6 +45,7 @@ our @EXPORT = qw(
   setup_kernel_logging
   init_debug
   run_supportconfig
+  prepare_ltp_git
 );
 
 sub loadtest_kernel {
@@ -98,6 +99,26 @@ sub get_ltp_version_file {
     return get_ltproot($want_32bit) . '/version';
 }
 
+sub prepare_ltp_git {
+    my $url = get_var('LTP_GIT_URL', 'https://github.com/linux-test-project/ltp');
+    my $rel = get_var('LTP_RELEASE');
+    my $prefix = get_ltproot();
+    my $configure = "./configure --prefix=$prefix";
+    my $extra_flags = get_var('LTP_EXTRA_CONF_FLAGS', '--with-open-posix-testsuite --with-realtime-testsuite');
+
+    $rel = "-b $rel" if ($rel);
+
+    script_run('rm -rf ltp');
+    my $ret = script_run("git clone -q --depth 1 $url $rel ltp", timeout => 360);
+    if (!defined($ret) || $ret) {
+        assert_script_run("git clone -q $url $rel ltp", timeout => 360);
+    }
+    assert_script_run 'cd ltp';
+    assert_script_run 'make autotools';
+    assert_script_run 'pwd';
+    assert_script_run("$configure $extra_flags", timeout => 300);
+}
+
 sub log_versions {
     my $report_missing_config = shift;
     my $kernel_pkg = is_jeos || get_var('KERNEL_BASE') ? 'kernel-default-base' :
@@ -113,6 +134,14 @@ sub log_versions {
     assert_script_run("curl -sS -o /tmp/$ver_linux $script_url");
     assert_script_run("chmod u+x /tmp/$ver_linux", timeout => 300);
     assert_script_run("for i in /opt/ltp*; do cp -v /tmp/$ver_linux \$i; done", timeout => 300);
+
+    prepare_ltp_git;
+    my $foo = 'irqbalance01.c';
+    my $dir = 'testcases/kernel/irq/';
+    my $script_url = data_url("ltp/$foo");
+    assert_script_run("curl -sS -o /tmp/$foo $script_url");
+    assert_script_run("cp -v /tmp/$foo $dir", timeout => 300);
+    assert_script_run("echo $dir; ls -la $dir || true; pwd; cd $dir " . ' && pwd && make -j$(getconf _NPROCESSORS_ONLN) && make install V=1', timeout => 600);
 
     script_run("$run_cmd rpm -qi $kernel_pkg > $kernel_pkg_log 2>&1", timeout => 120);
     upload_logs($kernel_pkg_log, failok => 1);
